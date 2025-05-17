3ac1abd488e8cb0e233d05cde52ac18fd08562a9
Nicholas Wilkins <150397998+Darth-Volix@users.noreply.github.com>
2025-05-16 20:46:08 -0600

    Add getVehicleById function to retrieve vehicle data by ID and create vehicle.ejs view file

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -25,4 +25,19 @@ async function getInventoryByClassificationId(classification_id) {
   }
 }
 
-module.exports = {getClassifications, getInventoryByClassificationId}
+/* ***************************
+ *  Get all data for a specific vehcile in inventory
+ * ************************** */
+async function getVehicleById(inv_id) {
+  try {
+    const data = await pool.query(
+      `SELECT * FROM public.inventory WHERE inv_id = $1`,
+      [vehicle_id]
+    )
+    return data.rows[0]
+  } catch (error) {
+    console.error("getVehicleById error " + error)
+  }
+}
+
+module.exports = {getClassifications, getInventoryByClassificationId, getVehicleById}
