f6b21d128cdf56fab508e34695e245616045757d
Nicholas Wilkins <150397998+NWilkins95@users.noreply.github.com>
2025-05-20 21:47:54 -0600

    Add error handling to inventory routes for improved stability

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,12 +1,13 @@
 // Needed Resources 
 const express = require("express")
 const router = new express.Router() 
+const utilities = require("../utilities/index")
 const invController = require("../controllers/invController")
 
 // Route to build inventory by classification view
-router.get("/type/:classificationId", invController.buildByClassificationId);
+router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId));
 
 // Route to build vehicle detail view
-router.get("/detail/:inv_id", invController.buildByInventoryId);
+router.get("/detail/:inv_id", utilities.handleErrors(invController.buildByInventoryId));
 
 module.exports = router;
