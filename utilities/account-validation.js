d020084ee02ce99ad850df572564099d063d5a74
Nicholas Wilkins <150397998+NWilkins95@users.noreply.github.com>
2025-05-28 21:35:08 -0600

    Refactor account validation to include error checking for registration data

--- utilities/account-validation.js
+++ utilities/account-validation.js
@@ -3,8 +3,8 @@ const utilities = require(".")
 const { body, validationResult } = require("express-validator")
 const validate = {}
 
-/*  **********************************
- *  Registration Data Validation Rules
+/* **********************************
+ * Registration Data Validation Rules
  * ********************************* */
 validate.registationRules = () => {
   return [
@@ -47,3 +47,27 @@ validate.registationRules = () => {
       .withMessage("Password does not meet requirements."),
   ]
 }
+
+/* ******************************
+ * Check data and return errors or continue to registration
+ * ***************************** */
+validate.checkRegData = async (req, res, next) => {
+  const { account_firstname, account_lastname, account_email } = req.body
+  let errors = []
+  errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    let nav = await utilities.getNav()
+    res.render("account/register", {
+      errors,
+      title: "Registration",
+      nav,
+      account_firstname,
+      account_lastname,
+      account_email,
+    })
+    return
+  }
+  next()
+}
+
+module.exports = validate
