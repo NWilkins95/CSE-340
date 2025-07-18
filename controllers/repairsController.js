24137655d685b8440bedfaa8ac239f758a177fb6
Nicholas Wilkins <150397998+NWilkins95@users.noreply.github.com>
2025-07-18 14:07:21 -0600

    Enhance add repair view by including navigation and improving error handling

--- controllers/repairsController.js
+++ controllers/repairsController.js
@@ -6,13 +6,19 @@ const repairsController = {}
  *  Build add repair view
  * ************************** */
 repairsController.buildAddRepair = async (req, res) => {
+  const nav = await utilities.getNav()
   const inv_id = req.params.inv_id
   const inventoryItem = await repairsModel.getRepairsByInventoryId(inv_id)
   if (!inventoryItem || inventoryItem.length === 0) {
     return res.status(404).send("Inventory item not found")
   }
-  res.render("repairs/add-repair", { title: "Add Repair", inv_id: inv_id })
+  res.render("repairs/add-repair", { 
+    title: "Add Repair", 
+    inv_id: inv_id, 
+    nav: nav 
+  })
 }
+
 
 
 module.exports = repairsController
