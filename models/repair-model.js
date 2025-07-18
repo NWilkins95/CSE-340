9527f9c5d60f23466fc2fa175ac5d53e6f1315ff
Nicholas Wilkins <150397998+NWilkins95@users.noreply.github.com>
2025-07-18 12:40:25 -0600

    Update repairs table to change repair_date type to character varying; add repair model with CRUD operations

---
+++ models/repair-model.js
@@ -0,0 +1,59 @@
+const pool = require("../database/index.js")
+
+/* ***************************
+ *  Get all repair data
+ * ************************** */
+const getAllRepairs = async () => {
+    const query = `
+        SELECT *
+        FROM public.repairs
+    `;
+    const { rows } = await pool.query(query);
+    return rows;
+};
+
+/* ***************************
+ *  Get repairs by inventory ID
+ * ************************** */
+const getRepairsByInventoryId = async (inv_id) => {
+    const query = `
+        SELECT *
+        FROM public.repairs
+        WHERE inv_id = $1
+    `;
+    const { rows } = await pool.query(query, [inv_id]);
+    return rows;
+};
+
+/* ***************************
+ *  Add a new repair record
+ * ************************** */
+const addRepair = async (inv_id, repair_description, repair_cost, repair_date) => {
+    const query = `
+        INSERT INTO public.repairs (inv_id, repair_description, repair_cost, repair_date)
+        VALUES ($1, $2, $3, $4)
+        RETURNING *
+    `;
+    const { rows } = await pool.query(query, [inv_id, repair_description, repair_cost, repair_date]);
+    return rows[0];
+};
+
+/* ***************************
+ *  Delete repair record by ID
+ * ************************** */
+const deleteRepairById = async (repair_id) => {
+    const query = `
+        DELETE FROM public.repairs
+        WHERE repair_id = $1
+        RETURNING *
+    `;
+    const { rows } = await pool.query(query, [repair_id]);
+    return rows[0];
+};
+
+module.exports = {
+    getAllRepairs,
+    getRepairsByInventoryId,
+    addRepair,
+    deleteRepairById
+};
