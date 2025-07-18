18e26aaf57586e07c6c2b8f2a7aa2dcbd7eecec3
Nicholas Wilkins <150397998+NWilkins95@users.noreply.github.com>
2025-07-18 13:51:55 -0600

    Refactor buildAddRepair method to retrieve inventory item by ID and handle not found case

--- controllers/repairsController.js
+++ controllers/repairsController.js
@@ -5,15 +5,14 @@ const repairsController = {}
 /* ***************************
  *  Build add repair view
  * ************************** */
-repairsController.buildAddRepair = async (req, res, next) => {
-  let nav = await utilities.getNav()
-  let inventory = await repairsModel.getAllRepairs()
-  res.render("repairs/add-repair", {
-    title: "Add Repair",
-    nav,
-    inventory,
-    errors: null,
-  })
+repairsController.buildAddRepair = async (req, res) => {
+  const inv_id = req.params.inv_id
+  const inventoryItem = await repairsModel.getRepairsByInventoryId(inv_id)
+  if (!inventoryItem || inventoryItem.length === 0) {
+    return res.status(404).send("Inventory item not found")
+  }
+  res.render("repairs/add-repair", { title: "Add Repair", inv_id: inv_id })
 }
+
 
 module.exports = repairsController
